5411129b22caadea39956c9cdeccfaa1ae4f4fab
peterk8213 <peterwilliams8213@gmail.com>
2024-04-18 01:36:37 +0300

    initial commit

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
+axios.defaults.baseURL = 'https://job-api-zmaa.onrender.com/api/v1';
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem('user');
